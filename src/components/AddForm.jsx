06e23baa9d7fd2ba6e85ab34e559d56abb56edb3
Mayank Jha <mayankjha8274@gmail.com>
2025-12-15 00:49:07 +0530

    Done

---
+++ src/components/AddForm.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../features/todo/todoSlice";
+
+export default function AddForm() {
+    const [task, setTask] = useState("");
+    const dispatch = useDispatch();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        // Dispatch action to add todo
+        console.log("New Todo Task:", task);
+        dispatch(addTodo(task ));
+        setTask("");
+    };
+
+    return (
+        <>
+         <form onSubmit={submitHandler}>
+            <input type="text" onChange={(e) => setTask(e.target.value)}></input>
+            <button>Add Todo</button>
+         </form>
+        </>
+    ); 
+}
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,24 @@
 import { useSelector } from 'react-redux';
+import AddForm from './AddForm.jsx';
+import { useDispatch } from 'react-redux';
+import { deleteTodo } from '../features/todo/todoSlice';
 
 export default function Todo() {
     const todos =  useSelector((state) => state.todos);
     console.log(todos);
+    const dispatch = useDispatch();
+
+    const clickHandler = (id) => {
+        console.log("Delete button clicked", id);
+        dispatch(deleteTodo(id));
+    };
+
     return (
         <>
+            <AddForm />
             <h2>Todo List App</h2>
             <ul>
-                {todos.map((todo) => (<li key={todo.id}>{todo.task}</li>))}
+                {todos.map((todo) => (<li key={todo.id}>{todo.task} <button onClick={() => clickHandler(todo.id)}> Delete</button></li>))}
             </ul>
         </>
     );
